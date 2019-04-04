edf4206abda0f35da717778faf3705040c58eda0
Luis Guilherme de Souza <luis.guilherme@sysmap.com.br>
2019-04-04 14:02:23 -0300

    commit of project-2

--- main.js
+++ main.js
@@ -1,7 +1,29 @@
-alert("Teste");
+class List {
+  constructor() {
+    this.data = [];
+  }
 
-class Teste {
-  metodo() {}
-
-  outro() {}
+  add(data) {
+    this.data.push(data);
+    console.log(this.data);
+  }
 }
+
+class TodoList extends List {
+  constructor() {
+    super();
+    this.usuario = "Usuário";
+  }
+
+  mostrarUsuario() {
+    console.log(this.usuario);
+  }
+}
+
+const MinhaLista = new TodoList();
+
+document.getElementById("novotodo").onclick = function() {
+  MinhaLista.add("Novo todo");
+};
+
+MinhaLista.mostrarUsuario();
